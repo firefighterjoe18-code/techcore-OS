7fa27dea5a545e02df6a8301b3ad4d38937e7ed7
firefighterjoe18-code <firefighterjoe18@gmail.com>
2026-07-08 18:01:51 -0400

    Create techcore-welcome.sh

---
+++ system_files/usr/local/bin/techcore-welcome.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "====================================="
+echo "      Welcome to TechCore OS"
+echo "      Initializing Systems..."
+echo "====================================="
